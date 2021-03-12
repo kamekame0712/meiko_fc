cbc65ff836aa6625f946c1be68fe3875860fb098
kamei <s-kamei@chuoh-kyouiku.co.jp>
2021-03-12 15:07:34 +0900

    掛情報未登録対応

--- app/views/admin/owner/detail.php
+++ app/views/admin/owner/detail.php
@@ -64,62 +64,66 @@
 											<h4>買掛情報</h4>
 										</div>
 										<div class="card-body">
-											<dl class="confirm-list">
-												<dt>事業形態</dt>
-												<dd><?= $ADATA['corporation'] == '1' ? '法人' : '非法人' ?></dd>
+											<?php if( empty($ADATA) ): ?>
+												<p>買掛情報が登録されていません。</p>
+											<?php else: ?>
+												<dl class="confirm-list">
+													<dt>事業形態</dt>
+													<dd><?= $ADATA['corporation'] == '1' ? '法人' : '非法人' ?></dd>
 
-												<dt><?= $ADATA['corporation'] == '1' ? '法人名' : '代表者名' ?></dt>
-												<dd><?= $ADATA['corpo_name'] ?></dd>
+													<dt><?= $ADATA['corporation'] == '1' ? '法人名' : '代表者名' ?></dt>
+													<dd><?= $ADATA['corpo_name'] ?></dd>
 
-												<?php if( $ADATA['corporation'] == '1' ): ?>
-													<dt>代表者名</dt>
-													<dd><?= $ADATA['executive'] ?></dd>
-												<?php endif; ?>
+													<?php if( $ADATA['corporation'] == '1' ): ?>
+														<dt>代表者名</dt>
+														<dd><?= $ADATA['executive'] ?></dd>
+													<?php endif; ?>
 
-												<dt><?= $ADATA['corporation'] == '1' ? '法人郵便番号' : '代表者自宅郵便番号' ?></dt>
-												<dd><?= $ADATA['zip1'] ?>-<?= $ADATA['zip2'] ?></dd>
+													<dt><?= $ADATA['corporation'] == '1' ? '法人郵便番号' : '代表者自宅郵便番号' ?></dt>
+													<dd><?= $ADATA['zip1'] ?>-<?= $ADATA['zip2'] ?></dd>
 
-												<dt><?= $ADATA['corporation'] == '1' ? '法人住所' : '代表者自宅住所' ?></dt>
-												<dd><?= $CONF['pref'][$ADATA['pref']] ?><?= $ADATA['addr1'] ?><?= $ADATA['addr2'] ?></dd>
+													<dt><?= $ADATA['corporation'] == '1' ? '法人住所' : '代表者自宅住所' ?></dt>
+													<dd><?= $CONF['pref'][$ADATA['pref']] ?><?= $ADATA['addr1'] ?><?= $ADATA['addr2'] ?></dd>
 
-												<dt><?= $ADATA['corporation'] == '1' ? '代表電話番号' : '代表者電話番号' ?></dt>
-												<dd><?= $ADATA['tel1'] ?>-<?= $ADATA['tel2'] ?>-<?= $ADATA['tel3'] ?></dd>
+													<dt><?= $ADATA['corporation'] == '1' ? '代表電話番号' : '代表者電話番号' ?></dt>
+													<dd><?= $ADATA['tel1'] ?>-<?= $ADATA['tel2'] ?>-<?= $ADATA['tel3'] ?></dd>
 
-												<dt>FAX番号</dt>
-												<?php if( !empty($ADATA['fax1']) && !empty($ADATA['fax2']) && !empty($ADATA['fax3']) ): ?>
-													<dd><?= $ADATA['fax1'] ?>-<?= $ADATA['fax2'] ?>-<?= $ADATA['fax3'] ?></dd>
-												<?php else: ?>
-													<dd>（入力なし）</dd>
-												<?php endif; ?>
+													<dt>FAX番号</dt>
+													<?php if( !empty($ADATA['fax1']) && !empty($ADATA['fax2']) && !empty($ADATA['fax3']) ): ?>
+														<dd><?= $ADATA['fax1'] ?>-<?= $ADATA['fax2'] ?>-<?= $ADATA['fax3'] ?></dd>
+													<?php else: ?>
+														<dd>（入力なし）</dd>
+													<?php endif; ?>
 
-												<dt>ご請求先</dt>
-												<dd><?= $ADATA['bill_to'] == '1' ? '上記と同じ' : '上記とは別' ?></dd>
+													<dt>ご請求先</dt>
+													<dd><?= $ADATA['bill_to'] == '1' ? '上記と同じ' : '上記とは別' ?></dd>
 
-												<?php if( $ADATA['bill_to'] == '2' ): ?>
-													<dt>ご請求先名</dt>
-													<dd><?= $ADATA['bill_name'] ?></dd>
+													<?php if( $ADATA['bill_to'] == '2' ): ?>
+														<dt>ご請求先名</dt>
+														<dd><?= $ADATA['bill_name'] ?></dd>
 
-													<dt>ご請求先郵便番号</dt>
-													<dd><?= $ADATA['bill_zip1'] ?>-<?= $ADATA['bill_zip2'] ?></dd>
+														<dt>ご請求先郵便番号</dt>
+														<dd><?= $ADATA['bill_zip1'] ?>-<?= $ADATA['bill_zip2'] ?></dd>
 
-													<dt>ご請求先住所</dt>
-													<dd><?= $CONF['pref'][$ADATA['bill_pref']] ?><?= $ADATA['bill_addr1'] ?><?= $ADATA['bill_addr2'] ?></dd>
+														<dt>ご請求先住所</dt>
+														<dd><?= $CONF['pref'][$ADATA['bill_pref']] ?><?= $ADATA['bill_addr1'] ?><?= $ADATA['bill_addr2'] ?></dd>
 
-													<dt>ご請求先電話番号</dt>
-													<dd><?= $ADATA['bill_tel1'] ?>-<?= $ADATA['bill_tel2'] ?>-<?= $ADATA['bill_tel3'] ?></dd>
-												<?php endif; ?>
+														<dt>ご請求先電話番号</dt>
+														<dd><?= $ADATA['bill_tel1'] ?>-<?= $ADATA['bill_tel2'] ?>-<?= $ADATA['bill_tel3'] ?></dd>
+													<?php endif; ?>
 
-												<dt>決済方法</dt>
-												<dd><?= $ADATA['settlement_method'] == '1' ? '振込' : '口座引落' ?></dd>
+													<dt>決済方法</dt>
+													<dd><?= $ADATA['settlement_method'] == '1' ? '振込' : '口座引落' ?></dd>
 
-												<?php if( $ADATA['settlement_method'] == '1' ): ?>
-													<dt>振込み名義</dt>
-													<dd><?= $ADATA['transfer_name'] ?></dd>
-												<?php else: ?>
-													<dt>金融機関名</dt>
-													<dd><?= $ADATA['bank_name'] ?></dd>
-												<?php endif; ?>
-											</dl>
+													<?php if( $ADATA['settlement_method'] == '1' ): ?>
+														<dt>振込み名義</dt>
+														<dd><?= $ADATA['transfer_name'] ?></dd>
+													<?php else: ?>
+														<dt>金融機関名</dt>
+														<dd><?= $ADATA['bank_name'] ?></dd>
+													<?php endif; ?>
+												</dl>
+											<?php endif; ?>
 										</div>
 									</div> <!-- end of .card -->
 								</div>
